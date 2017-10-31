0ec75b417967130e19d4e8d776e7b46cbe25d01e
Jocelyn <jxj140130@utdallas.edu>
2017-10-31 14:41:36 -0500

    Added jump controls, removed sprint. Added resources

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,27 +5,47 @@ using UnityEngine.UI;
 public class PlayerControls : MonoBehaviour
 {
 	//player speed
-	public float speed = 5.0f;
+	private float speed;
+	private float jumpVel;
+	private float distFromGround;
+	private CapsuleCollider playerColl;
+	private Rigidbody playerRigBd;
 	public Text score;
 	private int counter;
 
 	//called before Update and after Awake
 	void Start ()
 	{
+		//initialize references to the player Collider and Rigidbody
+		playerColl = GetComponent<CapsuleCollider>();
+		distFromGround = playerColl.bounds.extents.y;
+		playerRigBd = GetComponent<Rigidbody>();
+		
 		//mouse setting: locked to center of the window. No longer visible on screen
 		Cursor.lockState = CursorLockMode.Locked;
 
+		speed = 7.0f;
+		jumpVel = 5.0f;
 		counter = 0;
 		DisplayScore ();
 		//in the unity editor, freeze rotation on x and z axes on the rigidbody on the player object
 	}
 
+	//returns true if the player is not in the air and false otherwise
+	bool isGrounded ()
+	{
+		return Physics.Raycast(transform.position, -Vector3.up, distFromGround + 0.1f);
+	}
+	
 	//called once per frame
 	void Update ()
 	{
 		//vertical and horizontal values controlled internally by unity
 		float translation = Input.GetAxis ("Vertical") * speed;		//along z axis, forwards and backwards
 		float strafe = Input.GetAxis ("Horizontal") * speed;		//along x axis, left and right
+		//how high to jump
+		float moveJump = Input.GetAxis ("Jump");
+		Vector3 jump = Vector3.up * jumpVel * moveJump;
 
 		//keeps movements smooth and in time w/ updates
 		translation *= Time.deltaTime;
@@ -38,16 +58,12 @@ public class PlayerControls : MonoBehaviour
 			Cursor.lockState = CursorLockMode.None;
 		}
 
-		//sprint if left shift is held down
-		if (Input.GetKey (KeyCode.LeftShift)) {
-			speed = 7.0f;
-		}
-		if (Input.GetKeyUp(KeyCode.LeftShift)) {
-			speed = 5.0f;
-		}
+		//no sprinting bc that doesn't make sense. Instead, add boost pickup
 
-		//jump when spacebar is pressed
-
+		//jump when spacebar is pressed. Can only jump when on the ground, no double-jumping
+		if (isGrounded()) {
+			playerRigBd.velocity = jump;
+		}
 	}
 
 	//handles collisions
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 Resources:
 Food assets
 https://www.assetstore.unity3d.com/en/#!/content/85884
+First person tutorial
+https://www.youtube.com/watch?v=blO039OzUZc
+First person jump controls
+http://answers.unity3d.com/questions/691664/why-do-i-have-all-these-bugs-in-my-first-person-co.html
